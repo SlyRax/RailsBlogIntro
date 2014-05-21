ecd2487cefb4a5863b2bdd05542b1a3b7b980800
William Manning <wmanning@guidewire.com>
2014-05-21 22:21:30 +0100

    Initial checkin, just some empty actions and a form

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '16b73726cfe9e43c014bd7a4589cec0746a6ae00f4c03b9c937cad8ebadc2494fb871a8b08255dfe38b965241deee0f35063fe8eb4f684670448135a5f7ff2b7'
