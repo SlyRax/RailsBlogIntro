4b00fd3e9781f79f8f4da15db9b00d02b7713300
William Manning <wmanning@guidewire.com>
2014-06-06 18:00:37 +0100

    Adding comments

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529160445) do
+ActiveRecord::Schema.define(version: 20140606165631) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -23,11 +23,11 @@ ActiveRecord::Schema.define(version: 20140529160445) do
   create_table "comments", force: true do |t|
     t.string   "commenter"
     t.text     "body"
-    t.integer  "articles_id"
+    t.integer  "article_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "comments", ["articles_id"], name: "index_comments_on_articles_id"
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
 
 end
